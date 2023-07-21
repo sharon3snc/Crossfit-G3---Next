9ff0398be8addf92050fbf3da956bfcb15533151
jlbrugnoni <jlbrugnoni@gmail.com>
2023-07-20 22:53:28 -0400

    Actualización de DB

--- DB/Query1.sql
+++ DB/Query1.sql
@@ -38,3 +38,39 @@ VALUES ('jose@gmail.com', '12345', 'Jose', 'Rodriguez', '1992-01-01', '123456789
 select * from clients;
 
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '12345';
+
+select * from employees;
+
+insert into employees (password, user_admin, name, surname, birthdate, email, phone)
+VALUES ('12345', true, 'Rocio', 'Jimenez', '1990-01-01', 'johndoe@example.com', '1234567890');
+
+insert into employees (password, user_admin, name, surname, birthdate, email, phone)
+VALUES ('12345', false, 'Antonio', 'Gomez', '1990-01-01', 'johndoe@example.com', '1234567890');
+
+CREATE TABLE classes (
+  class_id INT AUTO_INCREMENT PRIMARY KEY,
+  class_date DATE NOT NULL,
+  class_hour TIME NOT NULL,
+  duration INT NOT NULL,
+  class_name VARCHAR(255) NOT NULL,
+  number_spaces INT NOT NULL,
+  employee_id INT NOT NULL,
+  FOREIGN KEY (employee_id) REFERENCES employees (employee_id)
+);
+
+CREATE TABLE client_classes (
+  client_id INT NOT NULL,
+  class_id INT NOT NULL,
+  PRIMARY KEY (client_id, class_id),
+  FOREIGN KEY (client_id) REFERENCES clients (client_id),
+  FOREIGN KEY (class_id) REFERENCES classes (class_id)
+);
+
+insert into classes (class_date, class_hour, duration, class_name, number_spaces, employee_id)
+VALUES ('2023-07-28', '09:30:00', 60, 'Crossfit', 20, 1);
+
+insert into classes (class_date, class_hour, duration, class_name, number_spaces, employee_id)
+VALUES ('2023-07-28', '10:30:00', 60, 'Yoga', 20, 2);
+
+select * from classes;
+select * from employees;
--- DB/crossfit3g.sql
+++ DB/crossfit3g.sql
@@ -16,184 +16,124 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `asistencia`
+-- Table structure for table `classes`
 --
 
-DROP TABLE IF EXISTS `asistencia`;
+DROP TABLE IF EXISTS `classes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `asistencia` (
-  `id_cliente` int DEFAULT NULL,
-  `id_clase` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `classes` (
+  `class_id` int NOT NULL AUTO_INCREMENT,
+  `class_date` date NOT NULL,
+  `class_hour` time NOT NULL,
+  `duration` int NOT NULL,
+  `class_name` varchar(255) NOT NULL,
+  `number_spaces` int NOT NULL,
+  `employee_id` int NOT NULL,
+  PRIMARY KEY (`class_id`),
+  KEY `employee_id` (`employee_id`),
+  CONSTRAINT `classes_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `asistencia`
+-- Dumping data for table `classes`
 --
 
-LOCK TABLES `asistencia` WRITE;
-/*!40000 ALTER TABLE `asistencia` DISABLE KEYS */;
-/*!40000 ALTER TABLE `asistencia` ENABLE KEYS */;
+LOCK TABLES `classes` WRITE;
+/*!40000 ALTER TABLE `classes` DISABLE KEYS */;
+INSERT INTO `classes` VALUES (1,'2023-07-28','09:30:00',60,'Crossfit',20,1),(2,'2023-07-28','10:30:00',60,'Yoga',20,2);
+/*!40000 ALTER TABLE `classes` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `clase`
+-- Table structure for table `client_classes`
 --
 
-DROP TABLE IF EXISTS `clase`;
+DROP TABLE IF EXISTS `client_classes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `clase` (
-  `id_clase` int NOT NULL,
-  `fecha` date DEFAULT NULL,
-  `hora` date DEFAULT NULL,
-  `duracion` int DEFAULT NULL,
-  `nombre_clase` varchar(30) DEFAULT NULL,
-  `num_plazas` int DEFAULT NULL,
-  `id_empleado` int DEFAULT NULL,
-  PRIMARY KEY (`id_clase`)
+CREATE TABLE `client_classes` (
+  `client_id` int NOT NULL,
+  `class_id` int NOT NULL,
+  PRIMARY KEY (`client_id`,`class_id`),
+  KEY `class_id` (`class_id`),
+  CONSTRAINT `client_classes_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`),
+  CONSTRAINT `client_classes_ibfk_2` FOREIGN KEY (`class_id`) REFERENCES `classes` (`class_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `clase`
+-- Dumping data for table `client_classes`
 --
 
-LOCK TABLES `clase` WRITE;
-/*!40000 ALTER TABLE `clase` DISABLE KEYS */;
-/*!40000 ALTER TABLE `clase` ENABLE KEYS */;
+LOCK TABLES `client_classes` WRITE;
+/*!40000 ALTER TABLE `client_classes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `client_classes` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `cliente`
+-- Table structure for table `clients`
 --
 
-DROP TABLE IF EXISTS `cliente`;
+DROP TABLE IF EXISTS `clients`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `cliente` (
-  `id_cliente` int NOT NULL,
-  `contrasena` varchar(50) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `nombre` varchar(80) DEFAULT NULL,
-  `apellidos` varchar(100) DEFAULT NULL,
-  `fecha_nacimeinto` date DEFAULT NULL,
-  `telefono` varchar(20) DEFAULT NULL,
-  `fecha_alta` date DEFAULT NULL,
-  `nombre_emergencia` varchar(80) DEFAULT NULL,
-  `telefono_emergencia` varchar(20) DEFAULT NULL,
-  `nombre_tarifa` varchar(20) DEFAULT NULL,
-  `clases_restantes` int DEFAULT NULL,
-  PRIMARY KEY (`id_cliente`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `clients` (
+  `client_id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `birthdate` date NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `inscription_date` date NOT NULL,
+  `emergency_contact` varchar(255) NOT NULL,
+  `emergency_phone` varchar(255) NOT NULL,
+  `rate_id` int DEFAULT NULL,
+  `available_classes` int DEFAULT NULL,
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `cliente`
+-- Dumping data for table `clients`
 --
 
-LOCK TABLES `cliente` WRITE;
-/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+LOCK TABLES `clients` WRITE;
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
+INSERT INTO `clients` VALUES (1,'maria@gmail.com','12345','Maria','Hurtado','1990-01-01','1234567890','2023-06-29','Jane Doe','9876543210',1,5),(2,'jose@gmail.com','12345','Jose','Rodriguez','1992-01-01','1234567890','2023-06-29','Jane Doe','9876543210',1,5),(3,'maria@gmail.com','12345','Jose','Brugnoni','1990-01-01','1234567890','2023-06-29','Jane Doe','9876543210',1,5),(4,'jl@gmail.com','12345','Luis','Brugnoni','1990-01-01','1234567890','2023-06-29','Jane Doe','9876543210',1,5),(5,'jl@gmail.com','12345','Luis','Brugnoni','1990-01-01','1234567890','2023-06-29','Jane Doe','9876543210',1,5),(6,'juanperez@gmail.com','12345','Jose','Brugnoni','1992-09-28','123123412312','2023-07-02','Josew','12312312312',1,10),(7,'jl@gmail.com','12345','Jose Luis','Brugnoni','1990-01-01','1234567890','2023-06-29','Jane Doe','9876543210',1,5),(9,'juanperez@gmail.com','12345','Antonio','Trastullo','1992-09-28','123123412312','2023-07-03','Jose','12312312312',1,10),(10,'jl@gmail.com','12345','Manuel','Brugnoni','1992-09-28','123123412312','2023-07-03','Josew','12312312312',1,10),(11,'jl@gmail.com','12345','Trini','Hurtado','1992-09-28','123123412312','2023-07-03','Jose','12312312312',1,10),(12,'albalopez@gmail.com','12345','Maria Trinidad','Hurtado','1992-09-28','123123412312','2023-07-03','Josew','12312312312',1,10),(15,'lopezmario@gmail.com','12345','Mario','Lopez','1992-09-28','123123412312','2023-07-03','Jose','12312312312',1,10),(16,'elonmusk@gmai.com','12345','Elon','Musk','1970-12-12','123456789','2023-07-21','Elon','123456789',1,10);
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `empleado`
+-- Table structure for table `employees`
 --
 
-DROP TABLE IF EXISTS `empleado`;
+DROP TABLE IF EXISTS `employees`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `empleado` (
-  `id_empleado` int NOT NULL,
-  `constrasena` varchar(50) DEFAULT NULL,
-  `tipo_usuario` varchar(20) DEFAULT NULL,
-  `nombre` varchar(80) DEFAULT NULL,
-  `apellidos` varchar(100) DEFAULT NULL,
-  `fecha_nacimiento` date DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `telefono` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id_empleado`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `employees` (
+  `employee_id` int NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) NOT NULL,
+  `user_admin` tinyint(1) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `birthdate` date NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  PRIMARY KEY (`employee_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `empleado`
+-- Dumping data for table `employees`
 --
 
-LOCK TABLES `empleado` WRITE;
-/*!40000 ALTER TABLE `empleado` DISABLE KEYS */;
-/*!40000 ALTER TABLE `empleado` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `padece`
---
-
-DROP TABLE IF EXISTS `padece`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `padece` (
-  `nombre_patologia` varchar(100) DEFAULT NULL,
-  `id_cliente` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `padece`
---
-
-LOCK TABLES `padece` WRITE;
-/*!40000 ALTER TABLE `padece` DISABLE KEYS */;
-/*!40000 ALTER TABLE `padece` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `patologia`
---
-
-DROP TABLE IF EXISTS `patologia`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `patologia` (
-  `nombre_patologia` varchar(100) NOT NULL,
-  `descripcion` varchar(300) DEFAULT NULL,
-  PRIMARY KEY (`nombre_patologia`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `patologia`
---
-
-LOCK TABLES `patologia` WRITE;
-/*!40000 ALTER TABLE `patologia` DISABLE KEYS */;
-/*!40000 ALTER TABLE `patologia` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tarifas`
---
-
-DROP TABLE IF EXISTS `tarifas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tarifas` (
-  `nombre_tarifa` varchar(20) NOT NULL,
-  `precio` decimal(10,2) DEFAULT NULL,
-  `clases_mensuales` int DEFAULT NULL,
-  PRIMARY KEY (`nombre_tarifa`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tarifas`
---
-
-LOCK TABLES `tarifas` WRITE;
-/*!40000 ALTER TABLE `tarifas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tarifas` ENABLE KEYS */;
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+INSERT INTO `employees` VALUES (1,'12345',1,'Rocio','Jimenez','1990-01-01','johndoe@example.com','1234567890'),(2,'12345',0,'Antonio','Gomez','1990-01-01','johndoe@example.com','1234567890'),(4,'12345',1,'Jose Luis','Brugnoni','1992-09-28','jlbrugnoni@gmail.com','123456789');
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -205,4 +145,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-01 22:25:50
+-- Dump completed on 2023-07-20 22:53:01
